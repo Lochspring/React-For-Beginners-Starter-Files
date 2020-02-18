4c12a1923d7a231aa0853e7c7d26e1e7e993deba
Matt Dempster <matt.dempster@gmail.com>
2020-02-18 15:01:19 -0500

    Commit up to events

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NotFound = props => (
+  <header className="top">
+    <h1>File Not Found!!</h1>
+  </header>
+);
+
+// class Header extends React.Component {
+//   render() {
+//     return <></>;
+//   }
+// }
+
+export default NotFound;
---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { getFunName, slugify } from "../helpers.js";
+
+class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  selectStore = event => {
+    event.preventDefault();
+    let slug = slugify(this.myInput.current.value);
+    console.log(this);
+    this.props.history.push(`/store/${slug}`);
+  };
+
+  render() {
+    return (
+      <>
+        <form className="store-selector" onSubmit={this.selectStore}>
+          <h2>Please enter a form</h2>
+          <input
+            type="text"
+            ref={this.myInput}
+            required
+            placeholder="Store name"
+            defaultValue={getFunName()}
+          />
+          <button type="submit">Visit Store </button>
+        </form>
+      </>
+    );
+  }
+}
+
+export default StorePicker;
